f0ac5aa50335f5a7e934abb9f344790bc474b99e
juan saavedra <juans0520@gmail.com>
2026-06-03 08:36:09 -0500

    ascac

--- src/modules/plazbot-config/plazbot-config.module.ts
+++ src/modules/plazbot-config/plazbot-config.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PlaceBotConfig } from './entities/place-bot-config.entity';
 import { WhatsAppTemplate } from './entities/whatsapp-template.entity';
+import { Place } from '../places/entities/place.entity';
 import { PlaceBotConfigService } from './place-bot-config.service';
 import { WhatsAppTemplateService } from './whatsapp-template.service';
 import { PlazbotConfigController } from './plazbot-config.controller';
@@ -9,7 +10,7 @@ import { PlazBotModule } from '../plazbot/plazbot.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([PlaceBotConfig, WhatsAppTemplate]),
+    TypeOrmModule.forFeature([PlaceBotConfig, WhatsAppTemplate, Place]),
     PlazBotModule,
   ],
   providers: [PlaceBotConfigService, WhatsAppTemplateService],
